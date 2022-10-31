b743a42ecb49378648413b6f777ce62700dc4d25
robinhood8865 <robinhood8865@gmail.com>
2022-11-01 00:50:08 +0900

    deploy with api

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 var indexRouter = require("./index");
 var usersRouter = require("./users");
 
-// const apis = require("./apis");
+const apis = require("./apis");
 
 const routes = (app) => {
   app.use(cors());
   app.use("/", indexRouter);
   app.use("/users", usersRouter);
-  // app.use("/api", apis);
+  app.use("/api", apis);
 };
 
 module.exports = routes;
