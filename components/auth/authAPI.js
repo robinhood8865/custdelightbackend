2ab865363e67f15a852b994f67baf6e7e7fe238c
robinhood8865 <robinhood8865@gmail.com>
2022-11-01 17:35:55 +0900

    with route

--- components/auth/authAPI.js
+++ components/auth/authAPI.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const authController = require("./authController");
-// const { check, validationResult } = require("express-validator");
 
 const authAPI = express.Router();
 
-//
-authAPI.post("/signin", authController.signin);
-authAPI.post("/signup", authController.signup);
+// authAPI.post("/signin", authController.signin);
+// authAPI.post("/signup", authController.signup);
 
 module.exports = authAPI;
--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const apis = require("./apis");
 
-var indexRouter = require("./index");
-var usersRouter = require("./users");
-
-// var apis = require("./apis");
-const myroute = require("../myroute/myroute");
 const routes = (app) => {
   app.use(cors());
-  app.use("/", indexRouter);
-  app.use("/users", usersRouter);
-  // app.use("/api", apis);
-  app.use("/my", myroute);
+  app.use("api", apis);
 };
+
+// var indexRouter = require("./index");
+// var usersRouter = require("./users");
+
+// var apis = require("./apis");
+// const myroute = require("../myroute/myroute");
+// const routes = (app) => {
+//   app.use(cors());
+//   app.use("/", indexRouter);
+//   app.use("/users", usersRouter);
+//   // app.use("/api", apis);
+//   app.use("/my", myroute);
+// };
 
 module.exports = routes;
