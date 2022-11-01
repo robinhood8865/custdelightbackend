960f71688e59e2927e71f2c11bb799d023148a12
robinhood8865 <robinhood8865@gmail.com>
2022-11-01 09:07:40 +0900

    double dot

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,14 @@ const cors = require("cors");
 var indexRouter = require("./index");
 var usersRouter = require("./users");
 
-var apis = require("./apis");
-
+// var apis = require("./apis");
+const myroute = require("../myroute/myroute");
 const routes = (app) => {
   app.use(cors());
   app.use("/", indexRouter);
   app.use("/users", usersRouter);
-  app.use("/api", apis);
+  // app.use("/api", apis);
+  app.use("/my", myroute);
 };
 
 module.exports = routes;
