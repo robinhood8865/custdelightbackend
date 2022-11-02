bd315e80476f6dab004b070bc94daa5dbf2d0ff9
robinhood8865 <robinhood8865@gmail.com>
2022-11-02 09:11:02 +0900

    with route

--- components/users/userController.js
+++ components/users/userController.js
@@ -1,28 +1,27 @@
-const userService = require("./userService");
-const widgetController = require("../widgets/widgetController");
-const { ERROR } = require("../../enum");
+// const userService = require("./userService");
+// const widgetController = require("../widgets/widgetController");
+// const { ERROR } = require("../../enum");
 
 const createUser = async (data) => {
-  if (!data) {
-    console.log(ERROR.NO_DATA);
-    return ERROR.NO_DATA;
-  }
-  const isNew = await userService.isNewUser(data);
-  if (!isNew) {
-    console.log(ERROR.USER_EXIST);
-    return ERROR.USER_EXIST;
-  }
-  const widget = await widgetController.createWidget({});
-
-  const user = await userService.createUser({
-    ...data,
-    widgetId1: [widget._id],
-  });
-  console.log(
-    "🚀 ~ file: userController.js ~ line 22 ~ createUser ~ user",
-    user
-  );
-  return user;
+  // if (!data) {
+  //   console.log(ERROR.NO_DATA);
+  //   return ERROR.NO_DATA;
+  // }
+  // const isNew = await userService.isNewUser(data);
+  // if (!isNew) {
+  //   console.log(ERROR.USER_EXIST);
+  //   return ERROR.USER_EXIST;
+  // }
+  // const widget = await widgetController.createWidget({});
+  // const user = await userService.createUser({
+  //   ...data,
+  //   widgetId1: [widget._id],
+  // });
+  // console.log(
+  //   "🚀 ~ file: userController.js ~ line 22 ~ createUser ~ user",
+  //   user
+  // );
+  // return user;
 };
 
 // const findOneByFilter = async (filter) => {
