4ca6ddfe1648da40dcb2b30b9676db9f3c5f1c64
robinhood8865 <robinhood8865@gmail.com>
2022-11-02 10:25:42 +0900

    with route

--- components/memberships/membershipController.js
+++ components/memberships/membershipController.js
@@ -1,24 +1,24 @@
-// const membershipServcie = require("./memberShipService");
+const membershipServcie = require("./memberShipService");
 
-// const createMembership = async () => {
-//   const membership = await membershipServcie.createMembership({});
-//   return membership;
-// };
+const createMembership = async () => {
+  const membership = await membershipServcie.createMembership({});
+  return membership;
+};
 
-// const updateMembershipById = async (id, data) => {
-//   const membership = await membershipServcie.updateMembershipById(id, data);
-//   return membership;
-// };
+const updateMembershipById = async (id, data) => {
+  const membership = await membershipServcie.updateMembershipById(id, data);
+  return membership;
+};
 
-// const findOneById = async (id) => {
-//   const membership = await membershipServcie.findOneById(id);
-//   return membership;
-// };
+const findOneById = async (id) => {
+  const membership = await membershipServcie.findOneById(id);
+  return membership;
+};
 
 const membershipController = {
-  // createMembership,
-  // findOneById,
-  // updateMembershipById,
+  createMembership,
+  findOneById,
+  updateMembershipById,
 };
 
 module.exports = membershipController;
