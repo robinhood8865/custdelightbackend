e38995b1cc58eb28b38e583dd18c8aed306375d8
robinhood8865 <robinhood8865@gmail.com>
2023-01-09 13:29:39 +0900

    fix:stripe

--- build/widget-embed.js
+++ build/widget-embed.js
@@ -1,6 +1,6 @@
 var cssFile = document.createElement("link");
 cssFile.rel = "stylesheet";
-cssFile.href = "./widget/index.css";
+cssFile.href = "https://custdelight-3du3.onrender.com/widget/index.css";
 document.head.appendChild(cssFile);
 
 var elem = document.createElement("div");
@@ -10,7 +10,7 @@ document.body.appendChild(elem);
 
 var jsFile1 = document.createElement("script");
 jsFile1.type = "text/javascript";
-jsFile1.src = "./widget/index.js";
+jsFile1.src = "https://custdelight-3du3.onrender.com/widget/index.js";
 document.body.appendChild(jsFile1);
 
 // var currentURL = window.location.href;
